277ba6180030563ead19c8d3098ec6ef2395fd9a
Przemyslaw Baranski <pbaranski@marvell.com>
2025-09-09 13:17:30 +0200

    Default num_ops_ to 0

--- src/feeder/et_feeder_node.cpp
+++ src/feeder/et_feeder_node.cpp
@@ -9,6 +9,7 @@ ETFeederNode::ETFeederNode(std::shared_ptr<ChakraProtoMsg::Node> node) {
   this->name_ = node->name();
   this->runtime_ = node->duration_micros();
   this->is_cpu_op_ = 0;
+  this->num_ops_ = 0;
 
   if (node->has_inputs()) {
     this->inputs_values_ = static_cast<string>(node->inputs().values());
